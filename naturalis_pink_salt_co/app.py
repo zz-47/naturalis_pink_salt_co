7d7fe2ab6f8cbe711f568cfacb8057f562ca73d4
zz-47 <786roshaan007@gmail.com>
2025-09-12 16:55:34 +0500

    update 0.4.8

--- naturalis_pink_salt_co/app.py
+++ naturalis_pink_salt_co/app.py
@@ -261,5 +261,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
