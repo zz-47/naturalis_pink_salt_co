d9d7c541119bd2ef87541da48a3ca69eb8c035e9
zz-47 <786roshaan007@gmail.com>
2025-09-12 17:16:11 +0500

    update  0.4.8

--- naturalis_pink_salt_co/app.py
+++ naturalis_pink_salt_co/app.py
@@ -260,6 +260,7 @@ def logout():
     flash("You have been logged out.", "info")
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
